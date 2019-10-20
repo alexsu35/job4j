0c6acb08f2ee2c04bb2012292704937e2ab5d653
Alexsu35 <subbotin0035@gmail.com>
2019-10-20 10:26:32 +0300

    Исправление Существует ли треугольник

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -4,7 +4,8 @@ public class Triangle {
 
     public static boolean exist(double ab, double ac, double bc) {
         if ((ab+ac >= bc) && (ab+bc >= ac) && (bc+ac >= ab)) {
-            return true;
+          //  return true;
+              return (ab+ac >= bc) && (ab+bc >= ac) && (bc+ac >= ab);
         }
         return false;
     }
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertThat;
 public class TriangleTest {
     @Test
     public void whenExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);
+        boolean result = Triangle.exist(1.0, 1.0, 2.0);
         assertThat(result, is(true));
     }
     @Test
