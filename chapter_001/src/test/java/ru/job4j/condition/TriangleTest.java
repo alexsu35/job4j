64a5c7e4503cc2e49cf34056f202f183e670e461
Alexsu35 <subbotin0035@gmail.com>
2019-10-20 10:57:52 +0300

    Исправление Существует ли треугольник

--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -7,12 +7,12 @@ import static org.junit.Assert.assertThat;
 public class TriangleTest {
     @Test
     public void whenExist() {
-        boolean result = Triangle.exist(1.0, 1.0, 2.0);
+        boolean result = Triangle.exist(2.0, 2.0, 2.0);
         assertThat(result, is(true));
     }
     @Test
     public void whenExist2() {
-        boolean result = Triangle.exist(-2.0, -2.0, 2.0);
+        boolean result = Triangle.exist(1.0, 1.0, 2.0);
         assertThat(result, is(false));
     }
 }
