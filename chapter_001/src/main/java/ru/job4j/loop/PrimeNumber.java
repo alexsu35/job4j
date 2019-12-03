92f16ea23063b3e356eca3da57223eda4bb67c77
alexsu35 <subbotin0035@gmail.com>
2019-12-03 22:18:09 +0100

    5.6. Простые числа.[#175751] Исправление

--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -2,18 +2,12 @@ package ru.job4j.loop;
 
 public class PrimeNumber {
     public int calc(int finish) {
+        CheckPrimeNumber checkPrimeNumber = new CheckPrimeNumber();
         int count = 0;
         for (int i = 2; i <= finish; i++){
-            int checkNumber = 0;
-            for (int j = 1; j <= i; j++){
-                if (i % j == 0){
-                    checkNumber++;
-                }
-            }
-
-            if (checkNumber == 2){
+            if (checkPrimeNumber.check(i))
                 count++;
-            }
+
         }
         return count;
     }
