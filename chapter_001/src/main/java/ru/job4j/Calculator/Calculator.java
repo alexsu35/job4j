a2534588611ad5b64a32f7037fabbf93f2f195e8
alexsu35 <subbotin0035@gmail.com>
2020-01-06 23:13:10 +0300

    5.8. JavaDoc. Code convention[#175748]

--- chapter_001/src/main/java/ru/job4j/Calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/Calculator/Calculator.java
@@ -1,18 +1,53 @@
 package ru.job4j.Calculator;
 
+/**
+ * Class Calculate
+ * Класс для вычисления арифметических операций + - * /
+ * @author Alex Su.
+ * @since 27.12.2019
+ * @version 1
+ */
+
+/**
+ * Сложение
+ * @param first первый аргумент
+ * @param second второй аргумент
+ */
+
 public class Calculator {
     public static void add(double first, double second) {
         double result = first + second;
         System.out.println(first + "+" + second + "=" + result);
     }
+
+    /**
+     * Деление
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
+
     public static void div(double first, double second) {
         double result = first / second;
         System.out.println(first + "/" + second + "=" + result);
     }
+
+    /**
+     * Произведение
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
+
     public static void multiply(double first, double second) {
         double result = first * second;
         System.out.println(first + "*" + second + "=" + result);
     }
+
+    /**
+     * Вычитание
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
+
     public static void subtruck(double first, double second) {
         double result = first - second;
         System.out.println(first + "-" + second + "=" + result);
---
+++ chapter_001/src/main/java/ru/job4j/Calculator/package-info.java
@@ -0,0 +1,9 @@
+package ru.job4j.Calculator;
+
+/**
+ * Package for calculate task.
+ *
+ * @author Alexandr Subbotin (subbotin0035@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
