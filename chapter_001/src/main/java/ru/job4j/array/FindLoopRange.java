a8815dc291afe559bd28513e456b2ccdd9bc5212
alexsu35 <subbotin0035@gmail.com>
2020-03-30 20:14:41 +0300

    6.5. Поиск индекса в диапазоне (Исправлено)

--- chapter_001/src/main/java/ru/job4j/array/FindLoopRange.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoopRange.java
@@ -13,13 +13,11 @@ public class FindLoopRange {
      *
      * @param data - массив.
      * @param el - искомое значение.
-     * @param start - индекс с которого начинаем поиск.
-     * @param finish - индекс с которым заканчиваем поиск.
      * @return индекс элемента массива равного искомому значению.
      */
 
             public static int indexOf(int[] data, int el) {
-                int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+                int rst = -1;
 
                 for (int index = 0; index < data.length; index++) {
                     if (data[index] == el) {
@@ -30,11 +28,19 @@ public class FindLoopRange {
                 return rst;
             }
 
-
+    /**
+     * Method indexOf.
+     * Метод должен вернуть индекс элемента массива равного искомому значению в диапазоне.
+     * @param data - массив.
+     * @param el - искомое значение.
+     * @param start - индекс с которого начинаем поиск.
+     * @param finish - индекс, которым заканчиваем поиск.
+     * @return индекс элемента массива равного искомому значению.
+     */
         public static int indexOf(int[] data, int el, int start, int finish) {
-            int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+            int rst = -1;
 
-            for (int index = start; index < finish; index++) {
+            for (int index = start; index <= finish; index++) {
                 if (data[index] == el) {
                     rst = index;
                     break;
