89d957507502c3351bef4d2d97782d366e5cf256
alexsu35 <subbotin0035@gmail.com>
2020-03-30 20:16:29 +0300

    6.5.2. Поиск минимума в диапазоне (Исправлено)

--- chapter_001/src/main/java/ru/job4j/array/MinDiapason.java
+++ chapter_001/src/main/java/ru/job4j/array/MinDiapason.java
@@ -1,5 +1,14 @@
 package ru.job4j.array;
 
+/**
+ * Method MinDiapason.
+ * Метод должен вернуть минимальное число из определенного диапахона в маасиве.
+ *
+ * @param data - массив.
+ * @param start - начальное значение диапазона.
+ * @param finish - онечное значение диапазона.
+ * @return минимальный элемент в массиве.
+ */
 
 public class MinDiapason {
     public static int findMin(int[] data, int start, int finish) {
