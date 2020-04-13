34bab332c47cf55ab1818f006c6423015c3c5755
alexsu35 <subbotin0035@gmail.com>
2020-04-13 19:33:12 +0300

    6.6.1. Двухмерный массив. Вывод на консоль.

---
+++ chapter_001/src/main/java/ru/job4j/array/BarleyBreak.java
@@ -0,0 +1,33 @@
+package ru.job4j.array;
+
+/**
+ * BarleyBreak.
+ * Класс для заполнения двумерного массива.
+ *
+ * @author Aleksandr S. (subbotin0035@yandex.ru)
+ */
+
+public class BarleyBreak {
+
+    /**
+     * Method main.
+     * Метод заполняет массив значениями и выводит в консоль
+     */
+
+    public static void main(String[] args) {
+
+       int[][] arr = new int[3][3];
+       int count = 1;
+       for(int i = 0; i < 3; i++) {
+           for (int j = 0; j < 3; j++) {
+            arr[i][j] = count++;
+           }
+       }
+       for(int[] anArr: arr) {
+           for (int anAnArr : anArr) {
+              System.out.print(anAnArr + " ");
+           }
+           System.out.println();
+       }
+    }
+}
