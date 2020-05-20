696085ec5ca3485ff18cf2cda033a93acac2ebb5
alexsu35 <subbotin0035@gmail.com>
2020-05-20 17:39:32 +0300

    6.7. Квадратный массив заполнен true или false по диагоналям ИСПРАВЛЕНО

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -74,25 +74,13 @@ public class MatrixCheck {
 
     public static boolean isWin(char[][] board) {
         boolean result = false;
-        int horizontalCells = 0;
-        int verticalCells = 0;
-        for (int row = 0; row < board.length; row++) {
-            if (board[row][row] == 'X') {
-                for (int cell = 0; cell < board.length; cell++) {
-                    if (board[row][cell] == 'X') {
-                        horizontalCells++;
-                    }
-                    if (board[cell][row] == 'X') {
-                        verticalCells++;
-                    }
-                }
-                if (verticalCells == board.length || horizontalCells == board.length) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 'X') {
+                if (monoHorizontal(board, i) == true || monoVertical(board, i) == true) {
                     result = true;
                     break;
                 }
-                break;
             }
-
         }
         return result;
     }
