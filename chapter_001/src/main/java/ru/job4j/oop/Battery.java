e9952c3cc767e207ff578c8b8550212c1f55f687
alexsu35 <subbotin0035@gmail.com>
2021-01-25 20:27:44 +0300

    1.7. Магические сосуды. [#175881]

---
+++ chapter_001/src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,25 @@
+package ru.job4j.oop;
+
+public class Battery {
+
+    private int load;
+
+    public Battery(int size) {
+        this.load = size;
+    }
+
+    public void charge ( Battery another) {
+        this.load = this.load + another.load;
+        another.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery firstBattery = new Battery(85);
+        Battery secondBattery = new Battery(15);
+        System.out.println("firstBattery : " + firstBattery.load + ". secondBattery : " + secondBattery.load);
+        secondBattery.charge(firstBattery);
+        System.out.println("first : " + firstBattery.load + ". second : " + secondBattery.load);
+
+    }
+
+}
