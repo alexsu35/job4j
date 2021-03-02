85a395186bd6d87097c9198aabbee8e2e51cbf84
alexsu35 <subbotin0035@gmail.com>
2021-03-02 22:16:40 +0300

    Реализация профессий в коде [#175884]

---
+++ chapter_002/src/main/java/ru/job4j/inheritance/House.java
@@ -0,0 +1,15 @@
+package ru.job4j.inheritance;
+
+public class House {
+
+    private int height;
+    private int width;
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/inheritance/Programmer.java
@@ -0,0 +1,8 @@
+package ru.job4j.inheritance;
+
+public class Programmer extends Engineer {
+
+    public Program create(Program program) {
+        return null;
+    }
+}
