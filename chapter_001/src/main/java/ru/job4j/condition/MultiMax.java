dfc5d5d7742a58add2dff515ba9645166818a261
alexsu35 <subbotin0035@gmail.com>
2021-09-01 20:32:33 +0300

    Редактирование Multimax

--- chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -1,7 +1,7 @@
 package ru.job4j.condition;
 
 public class MultiMax {
-    public int max(int first, int second, int third, int fourth) {
+    public int max(int first, int second, int third) {
         int result = Math.max(first, Math.max(second, third));
         return result;
     }
