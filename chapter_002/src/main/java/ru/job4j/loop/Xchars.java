d4c5d37f692b511646c3b89316c81bb421d5cf72
alexsu35 <subbotin0035@gmail.com>
2021-09-30 22:10:05 +0300

    Икс в прямоугольнике

---
+++ chapter_002/src/main/java/ru/job4j/loop/Xchars.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+import java.util.*;
+
+public class Xchars {
+    public static void main(String[] args) {
+        System.out.println(drawCross(15));
+    }
+
+    public static String drawCross(int size) {
+        StringBuilder sb = new StringBuilder();
+        int gap = size;
+        for (int j = 0; j < size; j++) {
+            for (int i = 0; i < size; i++) {
+                if (i == size - gap || i == gap - 1 || i == 0 || j == 0 || i == size - 1 || j == size - 1) {
+                    sb.append('*');
+                } else {
+                    sb.append(' ');
+                }
+            }
+            sb.append('\n');
+            gap--;
+        }
+        return sb.toString();
+    }
+}
