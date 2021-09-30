a49a01e7b1a09ecfc2c6465b4b52b1cfd39f3896
alexsu35 <subbotin0035@gmail.com>
2021-09-30 22:17:27 +0300

    Изменение по логу 2

--- chapter_002/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_002/src/main/java/ru/job4j/loop/Paint.java
@@ -4,10 +4,8 @@ public class Paint {
     public static void main(String[] arg) {
         int count = 10;
 
-        // Внешний цикл
         for (int i = 0; i < count; i++) {
             for (int k = 0; k < count; k++) {
-                // Вот наше сложное условие
                 if (k == 0 || k == count - 1 || i == 0 || i == count - 1 || k == i) {
                     System.out.print("*");
                 } else {
