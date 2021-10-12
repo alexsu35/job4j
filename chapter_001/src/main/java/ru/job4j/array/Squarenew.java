0ce9d9b6e57fbc3cb88b7e374216e386b71e4a5a
alexsu35 <subbotin0035@gmail.com>
2021-10-12 21:19:00 +0300

    6.0.5. Массивы в тестах. [#226789] без комментариев

--- chapter_001/src/main/java/ru/job4j/array/Squarenew.java
+++ chapter_001/src/main/java/ru/job4j/array/Squarenew.java
@@ -1,21 +1,7 @@
 package ru.job4j.array;
 
-/**
- * Class Square
- * Класс для заполнения массива
- *
- * @author Aleksandr S.
- * @version 1
- * @since 09.10.2021
- */
-
 public class Squarenew {
-    /**
-     * Возведение в квадрат
-     *
-     * @param bound - количество чисел в массиве.
-     * @return Массив вычисленных значений.
-     */
+
     public static int[] calculate(int bound) {
         int[] rst = new int[bound];
         int index = 0;
@@ -26,9 +12,6 @@ public class Squarenew {
     }
 
     public static void main(String[] args) {
-        /**
-         * Вывод значений индексов массива в консоль
-         */
         int[] array = calculate(5);
         for (int index : array) {
             System.out.println(index);
--- chapter_001/src/test/java/ru/job4j/array/SquarenewTest.java
+++ chapter_001/src/test/java/ru/job4j/array/SquarenewTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class SquarenewTest {
+
     @Test
     public void whenBound3Then114() {
         int bound = 3;
