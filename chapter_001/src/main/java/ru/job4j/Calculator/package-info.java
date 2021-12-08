d86ee70d4a4e4d6a16137294daac31130d3c0d8d
alexsu35 <subbotin0035@gmail.com>
2021-12-08 23:07:19 +0200

    Изменения после CheckStyle2

--- chapter_001/src/main/java/ru/job4j/Calculator/package-info.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.Calculator;
-
-/**
- * Package for calculate task.
- *
- * @author Alexandr Subbotin (subbotin0035@yandex.ru)
- * @version $Id$
- * @since 0.1
- */
--- chapter_001/src/main/java/ru/job4j/carmanager/Color.java
+++
@@ -1,6 +0,0 @@
-package ru.job4j.carmanager;
-
-public enum Color {
-
-    WHITE, GREEN, BLACK, BLUE
-}
